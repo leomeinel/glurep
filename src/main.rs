b22876eb750639684fdc81cc17d50c8d05d3b5fd
Leopold Johannes Meinel <leo@meinel.dev>
2026-05-17 18:42:03 +0200

    cli: Configure PlotConfig from cli args
    This also slightly changes PlotConfig to make more sense. All fields in
    PlotConfig are now configurable.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{env, error::Error, path::PathBuf};
 
-use clap::{arg, command, value_parser};
+use clap::{ArgMatches, arg, command, value_parser};
 
 use crate::{deserialize::prelude::*, plot::prelude::*};
 
@@ -9,26 +9,90 @@ mod plot;
 mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = command!()
-        .arg(
-            arg!(
-                -i <INPUT_FILE> "Input file (csv)"
-            )
-            .required(true)
-            .value_parser(value_parser!(PathBuf)),
-        )
-        .arg(
-            arg!(
-                -o <OUTPUT_DIR> "Output directory"
-            )
-            .required(true)
-            .value_parser(value_parser!(PathBuf)),
-        )
-        .get_matches();
+    let args = arg_matches();
 
-    let input_path = matches.get_one::<PathBuf>("INPUT_FILE").unwrap();
+    let input_path = args.get_one::<PathBuf>("INPUT_FILE").unwrap();
     let readings = GlucoseReadingsMap::from_file_path(input_path)?;
 
-    let output_path = matches.get_one::<PathBuf>("OUTPUT_DIR").unwrap();
-    plot_to_svg(readings, output_path, PlotConfig::default())
+    let output_path = args.get_one::<PathBuf>("OUTPUT_DIR").unwrap();
+    let plot_config = PlotConfig::from(&args);
+    plot_to_svg(readings, output_path, plot_config)
+}
+
+/// All [`ArgMatches`] from arguments that were supplied to the program at runtime by the user.
+fn arg_matches() -> ArgMatches {
+    command!()
+        .arg(
+            arg!(--width [width] "Width of the output svgs in pixels")
+                .value_parser(value_parser!(u32))
+                .id("size_x"),
+        )
+        .arg(
+            arg!(--height [height] "Height of the output svgs in pixels")
+                .value_parser(value_parser!(u32))
+                .id("size_y"),
+        )
+        .arg(
+            arg!(--"min-y" [max_y] "Minimum y value in `mg/dL`")
+                .value_parser(value_parser!(u32))
+                .id("min_y_spec"),
+        )
+        .arg(
+            arg!(--"max-y" [max_y] "Maximum y value in `mg/dL`")
+                .value_parser(value_parser!(u32))
+                .id("max_y_spec"),
+        )
+        .arg(
+            arg!(--nx --"num-x" [num_labels_x] "Maximum number of x labels")
+                .value_parser(value_parser!(usize))
+                .id("num_labels_x"),
+        )
+        .arg(
+            arg!(--ny --"num-y" [num_labels_y] "Maximum number of y labels")
+                .value_parser(value_parser!(usize))
+                .id("num_labels_y"),
+        )
+        .arg(
+            arg!(--sx --"size-x" [label_size_x] "Size of x labels in pixels")
+                .value_parser(value_parser!(u32))
+                .id("label_size_x"),
+        )
+        .arg(
+            arg!(--sy --"size-y" [label_size_y] "Size of y labels in pixels")
+                .value_parser(value_parser!(u32))
+                .id("label_size_y"),
+        )
+        .arg(
+            arg!(--cfs [font_size] "Caption font size")
+                .value_parser(value_parser!(u32))
+                .id("caption_font_size"),
+        )
+        .arg(
+            arg!(-r --radius [radius] "Radius of a single point in pixels")
+                .value_parser(value_parser!(u32))
+                .id("point_radius"),
+        )
+        .arg(
+            arg!(--low [low_glucose_threshold] "Low glucose threshold")
+                .value_parser(value_parser!(u32))
+                .id("min_glucose_threshold"),
+        )
+        .arg(
+            arg!(--high [high_glucose_threshold] "High glucose threshold")
+                .value_parser(value_parser!(u32))
+                .id("max_glucose_threshold"),
+        )
+        .arg(
+            arg!(
+                <INPUT_FILE> "Input file (csv)"
+            )
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .arg(
+            arg!(
+                <OUTPUT_DIR> "Output directory"
+            )
+            .value_parser(value_parser!(PathBuf)),
+        )
+        .get_matches()
 }
--- src/plot.rs
+++ src/plot.rs
@@ -4,6 +4,7 @@ pub(crate) mod prelude {
 
 use std::{collections::HashSet, error::Error, ops::Range, path::PathBuf};
 
+use clap::ArgMatches;
 use jiff::{Span, civil::Time};
 use plotters::{prelude::*, style::full_palette::*};
 
@@ -12,51 +13,48 @@ use crate::{deserialize::prelude::*, utils::prelude::*};
 /// Error if an invalid time is processed from the csv.
 const ERR_INVALID_TIME: &str = "Invalid time. Aborting.";
 
+/// Chart margin in pixels.
+const CHART_MARGIN: u32 = 10;
+/// Spec of `x` axis in seconds.
+const X_SPEC: Range<u32> = 0..(24 * 3600);
 /// Description for the y axis.
 const Y_DESC: &str = "mg/dL";
 
 /// Config for plotting [`GlucoseReadingsMap`] to svg.
 #[derive(Clone, Debug)]
 pub(crate) struct PlotConfig {
-    /// Margin of the drawing area.
-    margin: u32,
-    /// Size of the drawing area `(x, y)`.
+    /// Size of the drawing area `(x, y)` in pixels.
     ///
     /// This is equivalent to the size of the resulting svg.
-    size: (u32, u32),
+    pub(crate) size: (u32, u32),
 
-    /// Ranges of axes `(x, y)`.
-    ///
-    /// ## Note
-    ///
-    /// - The range for x axis is in seconds.
-    /// - The range for y axis is in `mg/dL`.
-    axes_ranges: (Range<u32>, Range<u32>),
+    /// Spec of `y` axis in `mg/dL`.
+    pub(crate) y_spec: Range<u32>,
     /// Number of labels for axes `(x, y)`.
-    num_labels: (usize, usize),
+    pub(crate) num_labels: (usize, usize),
     /// Size of labels for axes `(x, y)` in pixels.
-    label_size: (u32, u32),
+    pub(crate) label_size: (u32, u32),
 
-    /// Font size for the caption.
-    caption_font_size: u32,
+    /// Caption font size.
+    pub(crate) caption_font_size: u32,
 
     /// Radius of a single plotted point in pixels.
-    point_radius: u32,
+    pub(crate) point_radius: u32,
 
     /// Glucose target range.
     ///
     /// ## Note
     ///
-    /// - anything in range is displayed in [`BLUE`].
-    /// - anything below the range is displayed in [`RED`].
-    /// - anything above the range is displayed in [`RED`].
-    glucose_target_range: Range<u32>,
+    /// - anything in range is displayed in [`CYAN_300`].
+    /// - anything below the range is displayed in [`RED_300`].
+    /// - anything above the range is displayed in [`ORANGE_300`].
+    pub(crate) glucose_threshold: Range<u32>,
 }
 impl PlotConfig {
     fn measurement_color(&self, measurement: u32) -> RGBColor {
-        if measurement <= self.glucose_target_range.start {
+        if measurement <= self.glucose_threshold.start {
             return RED_300;
-        } else if measurement < self.glucose_target_range.end {
+        } else if measurement < self.glucose_threshold.end {
             return CYAN_300;
         } else {
             return ORANGE_300;
@@ -66,18 +64,56 @@ impl PlotConfig {
 impl Default for PlotConfig {
     fn default() -> Self {
         Self {
-            margin: 10,
             size: (640, 480),
 
-            axes_ranges: (0..(24 * 3600), 0..300),
+            y_spec: 0..300,
             num_labels: (6, 8),
             label_size: (20, 40),
 
-            caption_font_size: 30,
+            caption_font_size: 18,
 
             point_radius: 4,
-            glucose_target_range: 80..180,
+            glucose_threshold: 80..180,
         }
+    }
+}
+impl From<&ArgMatches> for PlotConfig {
+    fn from(matches: &ArgMatches) -> Self {
+        let mut config = PlotConfig::default();
+        matches.get_one::<u32>("size_x").map(|&x| config.size.0 = x);
+        matches.get_one::<u32>("size_y").map(|&y| config.size.1 = y);
+        matches
+            .get_one::<u32>("min_y_spec")
+            .map(|&y| config.y_spec.start = y);
+        matches
+            .get_one::<u32>("max_y_spec")
+            .map(|&y| config.y_spec.end = y);
+        matches
+            .get_one::<usize>("num_labels_x")
+            .map(|&x| config.num_labels.0 = x);
+        matches
+            .get_one::<usize>("num_labels_y")
+            .map(|&y| config.num_labels.1 = y);
+        matches
+            .get_one::<u32>("label_size_x")
+            .map(|&x| config.label_size.0 = x);
+        matches
+            .get_one::<u32>("label_size_y")
+            .map(|&y| config.label_size.1 = y);
+        matches
+            .get_one::<u32>("caption_font_size")
+            .map(|&s| config.caption_font_size = s);
+        matches
+            .get_one::<u32>("point_radius")
+            .map(|&r| config.point_radius = r);
+        matches
+            .get_one::<u32>("min_glucose_threshold")
+            .map(|&l| config.glucose_threshold.start = l);
+        matches
+            .get_one::<u32>("max_glucose_threshold")
+            .map(|&h| config.glucose_threshold.end = h);
+
+        config
     }
 }
 
@@ -90,10 +126,7 @@ pub(crate) fn plot_to_svg(
     for (date, readings) in readings.0 {
         let output_path = output_path.join(format!("{}.svg", date));
         let root = SVGBackend::new(&output_path, config.size).into_drawing_area();
-        root.margin(config.margin, config.margin, config.margin, config.margin);
 
-        let x_spec = config.axes_ranges.0.clone();
-        let y_spec = config.axes_ranges.1.clone();
         let caption = date.to_string();
         let mut chart = ChartBuilder::on(&root)
             .caption(
@@ -102,7 +135,8 @@ pub(crate) fn plot_to_svg(
             )
             .x_label_area_size(config.label_size.0)
             .y_label_area_size(config.label_size.1)
-            .build_cartesian_2d(x_spec.clone(), y_spec)?;
+            .margin(CHART_MARGIN)
+            .build_cartesian_2d(X_SPEC, config.y_spec.clone())?;
         chart
             .configure_mesh()
             // FIXME: We should use fixed positions/deltas for labels. I however haven't found a way to do that.
@@ -118,15 +152,15 @@ pub(crate) fn plot_to_svg(
             .y_desc(Y_DESC)
             .draw()?;
 
-        let target_range = config.glucose_target_range.clone();
-        let x_points = [x_spec.start, x_spec.end];
+        let glucose_threshold = config.glucose_threshold.clone();
+        let x_points = &[X_SPEC.start, X_SPEC.end];
         chart.draw_series(LineSeries::new(
-            x_points.map(|x| (x, target_range.end)),
-            config.measurement_color(target_range.end),
+            x_points.map(|x| (x, glucose_threshold.end)),
+            config.measurement_color(glucose_threshold.end),
         ))?;
         chart.draw_series(LineSeries::new(
-            x_points.map(|x| (x, target_range.start)),
-            config.measurement_color(target_range.start),
+            x_points.map(|x| (x, glucose_threshold.start)),
+            config.measurement_color(glucose_threshold.start),
         ))?;
 
         let readings: HashSet<_> = readings
